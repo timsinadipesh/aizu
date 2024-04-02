a5aa1baf9d25931d2b5cf83a0c3a5f45d607129d
creasac2 <timsinadipesh77@gmail.com>
2024-04-02 13:50:09 +0545

    implemented structured programming

--- itp1/wk5/structured_programming.py
+++ itp1/wk5/structured_programming.py
@@ -1,29 +1,26 @@
-def call(n):
-    """
-    Print all the numbers from 1 to n that are 
-    either divisible by 3 or contain the digit 3
-    """
-    for i in range(1, n+1):
-        x = i
-        prin = False
+"""
+Takes an interget 'n' input from the command line
+Prints all the numbers from 1 to n that are 
+either divisible by 3 or contain the digit 3
+"""
+n = int(input())
+for i in range(1, n+1):
+    x = i
+    prin = False
 
-        # divisible by 3
-        if x % 3 == 0:
-            prin = True
-        else:
-            # loop until x is greater than 0
-            while x > 0:
-                # last digit is 3
-                if x % 10 == 3:
-                    prin = True
-                    break
-                # remove the last digit
-                x /= 10
-        
-        if prin:
-            print("", i, end="")
-    print()
-
-# usage
-for i in range(20, 60, 10):
-    call(i)
+    # divisible by 3
+    if x % 3 == 0:
+        prin = True
+    else:
+        # loop until x is greater than 0
+        while x > 0:
+            # last digit is 3
+            if x % 10 == 3:
+                prin = True
+                break
+            # remove the last digit
+            x //= 10
+    
+    if prin:
+        print(" " + str(i), end="")
+print()
