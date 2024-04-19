9871b240f89f0999994163ece4b0c89c69d965c7
creasac2 <timsinadipesh77@gmail.com>
2024-04-19 08:01:56 +0545

    commit

---
+++ itp1/wk6/matrix_vector_multiplication.py
@@ -0,0 +1,25 @@
+"""
+reads a n * m matrix A and a m * 1 vector b
+and prints their product Ab
+"""
+
+n, m = map(int, input().split())
+
+a = []
+for i in range(n):
+    a.append(list(map(int, input().split())))
+
+b = []
+for i in range(m):
+    b.append(int(input()))
+
+result = []
+for i in range(n):
+    total = 0
+    for j in range(m):
+        total += a[i][j] * b[j]
+    result.append(total)
+
+for val in result:
+    print(val)
+        
